4700adc1b0e4a6e40374412e603c6980f08a1b2e
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-05-31 13:31:49 +0300

    chore: playground

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+indexed radix sort yapacağız
+
+1 -> parsing
+2 ->
+3 ->
+4 ->
+5 ->
+6 ->
+7 ->
--- main.c
+++ main.c
@@ -2,8 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int	main(int argc, char **argv)
 {
+    int *a;
+    if (argc < 2)
+        return 0;
 
+    a = malloc(sizeof(int) * argc);
+    int i = 0;
+    while (i < argc)
+    {
+        a[i] = atoi(argv[i]);
+        i++;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", a[i]);
+    }
+    
+    
 	return (0);
 }
