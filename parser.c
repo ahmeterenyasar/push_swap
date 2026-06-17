0a604ee82b80f23a708c71610a7b479f301c567a
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-17 21:42:22 +0300

    chore: implementing base functions for parser section

--- parser.c
+++ parser.c
@@ -9,3 +9,16 @@ static int	is_digit(char c)
 {
 	return (c >= '0' && c <= '9');
 }
+
+static long	ft_strict_atol(const char **s, int *er_mg)
+{}
+
+int get_total_capacity(int argc, char **argv)
+{
+}
+
+/*
+normalde split kullanırdım ama saygıdeğer şizofren kurulun
+ bunu intihal olarak hayal edebileceğini düşünerekten double pointer
+ gibi pointer ilerleterek kontrol edeceğim. anladınız mı?
+*/
