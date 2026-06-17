74e330ab308a8b247929dcd3674fe3c5abd6214e
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-17 22:33:39 +0300

    feat: implementing a struct for watching the whole program for live coding section. making çakallık

--- push_swap.h
+++ push_swap.h
@@ -7,6 +7,22 @@
 #include <limits.h>
 
 
+/*
+burada çok güzel bir çakallık yapacağım yeni evaluation pagede
+live coding kısmında bizden yanlış hatırlamıyorsam operasyonları değil
+kaç adet yapıldığını yazdırmamızı istiyormuş akran öğrenmesiyle öğrendim bu bilgiyi.
+bende bunun için bir üst wrapper struct'ı koyacağım yani bütün bunlar ayrı bir pencerede
+izleniyormuş gibi düşünebilirsiniz
+*/
+
+typedef struct s_environment
+{
+	t_stack *a;
+	t_stack *b;
+	// bunların da adını değişek aq stack dedik diye intihal yerim belki idk
+	int operation_counter;
+	int print_mode; // -> tam olarak burası yazdırmamızı sağlayacak okkey kafamda öyle kurdum yani
+} t_env;
 
 typedef struct s_stack
 {
@@ -22,7 +38,7 @@ void	stack_free(t_stack *stack);
 int		is_valid_format(char *str);
 long	ft_atol(const char *str);
 int		get_total_capacity(int argc,char **argv);
-int parser(t_stack *a, int argc, char **argv);
+int		parser(t_stack *a, int argc, char **argv);
 
 
 #endif
