e4fabd7e6f9c1c9dc5d04a7b07387505553d0ee1
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-18 18:36:36 +0300

    feat: start radix_sort implementation

---
+++ sorting.c
@@ -0,0 +1,17 @@
+#include "push_swap.h"
+
+
+/*
+şimdi napacaz düşünelim önce bir kaç durum var onları ele alacaz:
+sıralı gelmiş olabilir bu 1
+şu 3-5 küçük sayılı durumda radix falan diğerleri de verimsiz çalışıyor o yüzden
+özel case eklicem i dont care yani
+şimdilik böyle
+*/
+
+void radix_sort(t_environment *env)
+{
+    int i;
+    int bit;
+
+}
