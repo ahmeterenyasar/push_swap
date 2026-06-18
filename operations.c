d95c0340db047ebe4091a7cf5dd95568e22756c9
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-18 16:28:49 +0300

    chore: creating the operations file and functions

---
+++ operations.c
@@ -0,0 +1,47 @@
+#include "push_swap.h"
+
+void swap(t_stack *stack)
+{
+    int temp;
+
+}
+
+void push(t_stack *stack)
+{
+
+}
+
+void rotate(t_stack *stack)
+{
+
+}
+
+void reverse_rotate(t_stack *stack)
+{
+
+}
+
+void sa(t_stack *stack)
+{
+
+}
+
+void sb(t_stack *stack)
+{
+
+}
+
+void ss(t_stack *stack)
+{
+
+}
+
+void pa(t_stack *stack)
+{
+
+}
+
+void pb(t_stack *stack)
+{
+
+}
