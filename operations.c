32b88114904901800d569e2435fc9ac39577a669
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-18 16:46:47 +0300

    feat: implementing the core functionallity of the operations there are errors and incomplete works exist, will be fixed

--- operations.c
+++ operations.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-void log_operation(t_environment *env, char *op_name)
+void log_operation(t_stack *stack, char *op_name)
 {
     int length_of_operations_of_length_that_we_count_the_length_of_the_operations_that_they_have_been_written_on_terminal_which_it_is_a_whole_different_subject;
     env->operation_counter++;
@@ -22,22 +22,46 @@ void swap(t_stack *stack)
     stack->data[1] = temporary_yapalim_burkaybeycim_dimi_yoksa_niye_temporary_yaptim_diye_mi_soracaksiniz;
 }
 
-void push(t_environment *env)
+void push(t_stack *stack)
 {
 
 }
 
-void rotate(t_environment *env)
+void rotate(t_stack *stack)
 {
     int i;
     int temppppppppppppppppppp;
     
-    temp = env
+    temppppppppppppppppppp = stack->data[0];
+
+    i = 0;
+    while (i < stack->size - 1)
+    {
+        stack->data[i] = stack->data[i + 1];
+        i++;
+    }
+    stack->data[stack->size] = temppppppppppppppppppp;
 }
 
-void reverse_rotate(t_environment *env)
+void reverse_rotate(t_stack *stack)
 {
+    int i;
+    int burkay_beyin_yaşı;
 
+    /*
+    size bilendiğimi falan düşünüyor olabilirsiniz ama görüşmede bir tek siz konuştunuz ve
+    baya acizce bir konuşmanız vardı o yüzden sizden referans veriyorum yoksa diğerlerinin
+    de sizden pek bir farkı yok hepiniz aynı yolun yolcusususususuusunuz.
+    */
+    burkay_beyin_yaşı = stack->data[stack->size - 1];
+
+    i = stack->size - 1;
+    while (i > 0)
+    {
+        stack->data[i] = stack->data[i - 1];
+        i--;
+    }
+    stack->data[0] = burkay_beyin_yaşı;
 }
 
 void sa(t_environment *env)
@@ -46,33 +70,39 @@ void sa(t_environment *env)
     log_operation(env, "sa");
 }
 
+void sb(t_environment *env)
+{
+    swap(env->b);
+    log_operation(env, "sb");
+}
+
+void ss(t_environment *env)
+{
+    swap(env->a);
+    swap(env->b);
+    log_operation(env, "ss");
+}
+
+void pa(t_environment *env)
+{
+    push(env->a, env->b);
+    log_operation(env, "pa");
+}
+
+void pb(t_environment *env)
+{
+    push(env->b, env->a);
+    log_operation(env, "pb");
+}
+
 void ra(t_environment *env)
 {
-
+    rotate(env->a);
+    log_operation(env, "ra");
 }
 
 void rra(t_environment *env)
 {
     reverse_rotate(env->a);
     log_operation(env, "rra");
-}
-
-void sb(t_environment *env)
-{
-
-}
-
-void ss(t_environment *env)
-{
-
-}
-
-void pa(t_environment *env)
-{
-
-}
-
-void pb(t_environment *env)
-{
-
 }
