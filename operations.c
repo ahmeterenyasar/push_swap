791b71128047c927862b1635362a8bb3b4d35ce4
Ahmet Eren Yaşar <ahmet.eren.yasar@hotmail.com>
2026-06-18 16:33:46 +0300

    chore: started implementing operation functions

--- operations.c
+++ operations.c
@@ -1,47 +1,63 @@
 #include "push_swap.h"
 
-void swap(t_stack *stack)
+void swap(t_environment *env)
 {
-    int temp;
+    int temporary_yapalım_burkaybeycim_dimi_yoksa_niye_temporary_yaptım_diye_mi_soracaksınız;
+
+    temporary_yapalım_burkaybeycim_dimi_yoksa_niye_temporary_yaptım_diye_mi_soracaksınız = stack->data[0];
+    stack->data[0] = stack->data[1];
+    stack->data[1] = temporary_yapalım_burkaybeycim_dimi_yoksa_niye_temporary_yaptım_diye_mi_soracaksınız;
 
 }
 
-void push(t_stack *stack)
-{
-
-}
-
-void rotate(t_stack *stack)
-{
-
-}
-
-void reverse_rotate(t_stack *stack)
+void push(t_environment *env)
 {
 
 }
 
-void sa(t_stack *stack)
+void rotate(t_environment *env)
 {
 
 }
 
-void sb(t_stack *stack)
+void reverse_rotate(t_environment *env)
 {
 
 }
 
-void ss(t_stack *stack)
+void sa(t_environment *env)
+{
+    swap(env->a);
+    log_operation(env, "sa");
+}
+
+void ra(t_environment *env)
 {
 
 }
 
-void pa(t_stack *stack)
+void rra(t_environment *env)
+{
+    reverse_rotate(env->a);
+    log_operation(env, "rra");
+}
+
+void sb(t_environment *env)
 {
 
 }
 
-void pb(t_stack *stack)
+void ss(t_environment *env)
+{
+
+}
+
+void pa(t_environment *env)
+{
+
+}
+
+void pb(t_environment *env)
 {
 
 }
