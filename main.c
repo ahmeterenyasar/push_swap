288276fbb8c78017845939c0a2b99b890a7e8bad
ahmeterenyasar <ahmet.eren.yasar@hotmail.com>
2026-06-23 18:24:52 +0300

    chore: testing and formatting files

--- main.c
+++ main.c
@@ -1,25 +1,22 @@
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	t_environment	env;
 	int				capacity;
 
 	if (argc < 2)
 		return (0);
-
 	capacity = get_total_capacity(argc, argv);
 	if (capacity <= 0)
 	{
 		write(2, "Error\n", 6);
 		return (1);
 	}
-
 	env.a = stack_init(capacity);
 	env.b = stack_init(capacity);
 	if (!env.a || !env.b)
 		return (1);
-
 	env.operation_counter = 0;
 	env.print_mode = 1;
 	if (!parser(env.a, argc, argv) || !has_no_copies(env.a))
@@ -33,6 +30,5 @@ int main(int argc, char **argv)
 	radix_sort(&env);
 	stack_free(env.a);
 	stack_free(env.b);
-
 	return (0);
 }
